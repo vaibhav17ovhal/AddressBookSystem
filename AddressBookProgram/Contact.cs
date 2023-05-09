696b7f2eed3cb6b410e0a817506655a98d3df24c
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-09 18:12:35 +0530

    [Vaibhav] Add:Creating the details of a contact

---
+++ AddressBookProgram/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    public class Contact
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PhoneNumber;
+        public string Email;
+    }
+}
+
